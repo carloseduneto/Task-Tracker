27440860255a9e27d6f08453b88bc1d1d8a689ea
Carlos Neto <cecn2710@gmail.com>
2025-07-23 22:07:29 -0300

    Create a JSON file and manipulate this instance

---
+++ tasks.py
@@ -0,0 +1,64 @@
+'''
+Structure:
+Name
+Date
+Status -> Done, In Progress, Not Done
+'''
+
+import json
+data = {
+    "name": "Play games",
+    "when":"12/05/2025",
+    "progress":"in progress"
+}
+
+def writeJson():
+    try:
+        with open('output.json', 'r') as json_file:
+            json.dump(data, json_file, indent=4)
+        print("JSON file 'output.json' created successfully.")
+    except IOError as e:
+        print(f"Error writing to file: {e}")
+
+def readJson():
+    try:
+        with open("output.json", 'r') as json_file:
+            try:
+                existing_data = json.load(json_file)
+            except json.JSONDecodeError:
+                existing_data = []  # Handle cases where the file is empty or invalid JSON
+    except FileNotFoundError:
+        existing_data = []
+    print(existing_data)
+
+
+
+'''    if isinstance(existing_data, list):
+        if isinstance(new_data, list):
+            existing_data.extend(new_data)
+        else:
+            existing_data.append(new_data)
+    elif isinstance(existing_data, dict):
+        if isinstance(new_data, dict):
+            existing_data.update(new_data)
+        else:
+            raise TypeError("Cannot concatenate a list with a dictionary.")
+    else:
+        raise TypeError("Unsupported data type for concatenation.")
+'''
+
+
+
+readJson()
+
+# Add, Update, and Delete tasks
+
+# Mark a task as in progress or done
+
+# List all tasks
+
+# List all tasks that are done
+
+# List all tasks that are not done
+
+# List all tasks that are in progress
