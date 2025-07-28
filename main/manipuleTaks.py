350f73d3d417468821b79ac25de2a3bccaf0144d
Carlos Eduardo Neto <112590775+carloseduneto@users.noreply.github.com>
2025-07-28 17:19:05 -0300

    Menu navigation and options

--- main/manipuleTaks.py
+++ main/manipuleTaks.py
@@ -5,6 +5,12 @@
 # List all tasks that are in progress
 
 import json
+import os
+
+class color:
+    BOLD = '\033[1m'
+    GREEN = '\033[32m'
+    END = '\033[0m'
 
 tasks = [{
     "name": "Play games",
@@ -16,9 +22,9 @@ tasks = [{
 # json.dumps(tasks)
 # print(tasks)
 
-name = "Nobody stop nobody"
+name = "Another Task"
 date = "26/07/2025"
-priority = 4
+priority = 2
 
 taskRegistry = {
     "name": name,
@@ -52,7 +58,7 @@ def addNewTask(task, allTasks):
     return allTasks
 
 #Add new Task to JSON
-def addTaskToJson():
+def addTaskToJson(taskRegistry):
     listOfTasks = listAllTasks()
     listOfTasksAdded = addNewTask(taskRegistry, listOfTasks)
     createFileJson(listOfTasksAdded)
@@ -74,7 +80,7 @@ def updateTasksJson(taskSelect, newNameToTask, parameterToUpdate):
 
     createFileJson(listOfTasks)
 
-taskSelect = "Abobora"
+taskSelect = "Frio"
 
 #Delete tasks
 def deleteTaskJson(taskSelect):
@@ -85,4 +91,79 @@ def deleteTaskJson(taskSelect):
             print("Deleted Task!")
     createFileJson(listOfTasks)
 
-deleteTaskJson(taskSelect)
+# addTaskToJson(taskRegistry)
+
+# deleteTaskJson(taskSelect)
+
+def clear_terminal():
+    """Clears the terminal screen."""
+    # For Windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+    # For macOS and Linux
+    else:
+        _ = os.system('clear')
+
+option = ""
+while (option != 6):
+    print("Option: " + str(option))
+    print(
+        color.BOLD+ "TASK TRACKER\n" + color.END +
+        "1. Add task\n" +
+        "2. Update task\n"+
+        "3. Delete task\n"+
+        "4. List tasks\n" +
+        "5. Change task status\n"+
+        "6. Exit"
+    )
+    option = int(input(">>"))
+    clear_terminal()
+
+    if(option == 1):
+        print(color.BOLD+ "TASK TRACKER - ADD TASK\n" + color.END)
+        taskName = input("Task name: ")
+        date = input("Date: ")
+        priority = int(input("Priority: "))
+
+        clear_terminal()
+        taskRegistry = {
+            "name": taskName,
+            "date": date,
+            "progress": "not done",
+            "priority": priority
+        }
+
+        addTaskToJson(taskRegistry)
+
+        optionAdd = ""
+        while (optionAdd != 0):
+            print(color.BOLD + color.GREEN + "✅ TASK SUCESSFUL ADDED" + color.END +
+                "\n0. Back"
+                )
+            optionAdd = int(input(">>"))
+            clear_terminal()
+
+
+print(
+    color.BOLD+ "TASK TRACKER - TO LIST\n" + color.END +
+    "0. Back\n"
+    "1. List all tasks\n" +
+    "2. List done tasks\n" +
+    "3. List not done tasks\n" +
+    "4. List in progress tasks\n"
+)
+
+print(
+    color.BOLD+ "TASK TRACKER - CHANGE TASK STATUS\n" + color.END +
+    "0. Back\n" +
+    '1. Change in progress\n' +
+    "2. Change done"
+)
+
+
+
+
+input(">>")
+
+clear_terminal()
+
